c862c2f013bdba0e36a803b42415cbf7681db950
Scs143 <rasel@shortcircuit.science>
2024-11-03 22:09:49 +0600

    Updated

--- DRF/DRF/urls.py
+++ DRF/DRF/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from DRFApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('drf/', views.DRFQuest_info),
+    path('drf/<int:pk>', views.DRFQuest_ins)
 ]
--- DRF/DRFApp/views.py
+++ DRF/DRFApp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import DRFQuest
+from .serializers import DRFSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+# DRFQuest
+def DRFQuest_info(request):
+    # complex data 
+    drf = DRFQuest.objects.all()
+    # Convert python dic it's mean native data 
+    serializer = DRFSerializer(drf, many=True)
+    # Jason render
+    jason_render = JSONRenderer().render(serializer.data)
+    # Json Sent to user 
+    return HttpResponse(jason_render, content_type='application/json')
+
+
+
+# Model Instance to see the model instance singly
+def DRFQuest_ins(request, pk):
+    # complex data 
+    drf = DRFQuest.objects.get(id=pk)
+    # Convert python dic it's mean native data 
+    serializer = DRFSerializer(drf, many=False)
+    # Jason render
+    jason_render = JSONRenderer().render(serializer.data)
+    # Json Sent to user 
+    return HttpResponse(jason_render, content_type='application/json')
