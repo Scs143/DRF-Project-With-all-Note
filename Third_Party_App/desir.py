434730e23a484bbdcf65df36acc18478525fc281
Scs143 <rasel@shortcircuit.science>
2024-11-04 16:35:56 +0600

    Updated

---
+++ Third_Party_App/desir.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+URL = "http://127.0.0.1:8000/drfcreate/"
+
+
+data = {
+    'teacher_name': 'Rahul',
+    'course_name': "Physics",
+    'course_duration': 3,
+    'seat': 20
+}
+
+json_data = json.dumps(data) # dumps convert python data into json data
+r = requests.post(url=URL, data=json_data) # post request
+data = r.json()
+print(data)
